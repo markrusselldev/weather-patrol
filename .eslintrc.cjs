d2b9be94bcc7510b3ff5bf40ba714425c40eb31b
markrusselldev <mark@markrussell.dev>
2024-08-18 14:07:12 -0600

    Wrap subject at 50 characters--------------------> Cleanup package.json and update npm packages, fly.io config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,3 +1,5 @@
+const isProduction = process.env.NODE_ENV === 'production';
+
 module.exports = {
   root: true,
   env: { browser: true, es2020: true, node: true },
@@ -5,7 +7,9 @@ module.exports = {
   ignorePatterns: ["dist", ".eslintrc.cjs"],
   parserOptions: { ecmaVersion: "latest", sourceType: "module" },
   settings: { react: { version: "18.2" } },
-  plugins: ["react-refresh"],
+  plugins: [
+    !isProduction && "react-refresh"  // Include react-refresh only in development
+  ].filter(Boolean),
   rules: {
     "react/jsx-no-target-blank": "off",
     "react-refresh/only-export-components": ["warn", { allowConstantExport: true }]
